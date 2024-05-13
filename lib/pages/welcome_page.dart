cffea0a2592577054f4efe51b8e6aa6431b76c7a
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-05-13 14:27:56 +0500

    Updated Code for RESPONSIVE BUTTON

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:travel_app_bloc/app_constants/colors.dart';
 import 'package:travel_app_bloc/app_constants/dimensions.dart';
 import 'package:travel_app_bloc/widgets/big_text.dart';
+import 'package:travel_app_bloc/widgets/responsive_button.dart';
 import 'package:travel_app_bloc/widgets/small_text.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -56,9 +57,7 @@ class _WelcomePageState extends State<WelcomePage> {
                           text: "Mountain",
                           size: Dimensions.font20 * 2,
                         ),
-                        SizedBox(
-                          height: Dimensions.height20,
-                        ),
+                        SizedBox(height: Dimensions.height20),
                         SizedBox(
                           width: Dimensions.width30 * 10,
                           child: SmallText(
@@ -66,6 +65,10 @@ class _WelcomePageState extends State<WelcomePage> {
                                 "We spent the afternoon hiking around the lake. She hiked 10 miles in the hot desert sun. We hiked some of the shorter trails",
                             size: Dimensions.font20,
                           ),
+                        ),
+                        SizedBox(height: Dimensions.height45),
+                        ResponsiveButton(
+                          width: Dimensions.width30 * 5,
                         ),
                       ],
                     )
--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_bloc/app_constants/colors.dart';
+import 'package:travel_app_bloc/app_constants/dimensions.dart';
 
+// ignore: must_be_immutable
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+
+  ResponsiveButton({super.key, this.isResponsive, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: Dimensions.height30 * 2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(Dimensions.radius20 / 2),
+        color: AppColor.whiteColor,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/button-one.png",
+            height: Dimensions.height30,
+          ),
+        ],
+      ),
+    );
+  }
+}
